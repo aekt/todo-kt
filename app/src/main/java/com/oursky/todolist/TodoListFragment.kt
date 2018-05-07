2f3ad5b675ee72e0b4b463745c3f2e8e07f8bc8f
Johnny Kao <kaojohnny@gmail.com>
2018-05-07 11:09:11 +0800

    Move fab button to ToDoListFragment

--- app/src/main/java/com/oursky/todolist/TodoListFragment.kt
+++ app/src/main/java/com/oursky/todolist/TodoListFragment.kt
@@ -1,10 +1,12 @@
 package com.oursky.todolist
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_todo_list.*
 
 class TodoListFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater?,
@@ -13,5 +15,15 @@ class TodoListFragment: Fragment() {
         // Inflate the layout for this fragment
         return inflater?.inflate(R.layout.fragment_todo_list,
                 container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
     }
 }
